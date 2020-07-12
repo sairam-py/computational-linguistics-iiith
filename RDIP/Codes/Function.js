6ff69a25814e80bb17a21468b3deb9a010ca8f7a
sairam-py <sairam.kollimarla@yahoo.com>
2020-07-12 12:35:20 +0530

    reform_sentence , gui updated , on press the word vanishes functionalities are added

--- RDIP/Codes/Function.js
+++ RDIP/Codes/Function.js
@@ -1,63 +1,141 @@
-var sentanceEnglish = [
-    ['John', 'ate', 'an', 'apple', 'before', 'afternoon'],
-    ['some', 'students', 'like', 'to', 'study', 'at', 'night'],
-    ['John', 'and', 'Mary', 'went', 'to', 'church'],
-    ['John', 'went', 'to', 'church', 'after', 'eating'],
-    ['did', 'he', 'go', 'to', 'market'],
-    ['the', 'woman', 'who', 'called', 'my', 'sister', 'sells', 'cosmetics'],
-    ['John', 'goes', 'to', 'the', 'library', 'and', 'studies'],
-    ['John', 'ate', 'an', 'apple', 'so', 'did', 'she'],
-    ['the', 'teacher', 'returned', 'the', 'book', 'after', 'she', 'noticed', 'the', 'error'],
-    ['I', 'told', 'her', 'that', 'I', 'bought', 'a', 'book', 'yesterday']
-];
-var sentanceHindi = [
-    ['राम', 'और', 'श्याम', 'बाजार', 'गयें'],
-    ['राम', 'सोया', 'और', 'श्याम', 'भी'],
-    ['मैंने', 'उसे', 'बताया', 'कि', 'राम', 'सो', 'रहा', 'है'],
-    ['राम', 'खाकर', 'सोया'],
-    ['बिल्लियों', 'को', 'मारकर', 'कुत्ता', 'सो', 'गया'],
-    ['एक', 'लाल', 'किताब', 'वहाँ', 'है'],
-    ['एक', 'बड़ी', 'सी', 'किताब', 'वहाँ', 'है']
-];
-function selectOptions() {
-    var selection = document.getElementById("select").value;
-    var text = "";
-    if(selection == 'english' ){
-        text = "Form a sentence (Declarative or Interrogative or any other type) from the given words<br>(select the buttons in proper order)";
-        randomEnglish();
+var array1 = [["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"],
+["some students like to study in the night","at night some students like to study"],
+["John and Mary went to church","Mary and John went to church"],
+["John went to church after eating","after eating John went to church","John after eating went to church"],
+["did he go to market","he did go to market"],
+["the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics"],
+["John goes to the library and studies","John studies and goes to the library"],
+["John ate an apple so did she","she ate an apple so did John"],
+["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"],
+["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"]];
+
+var array2 = [["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"],
+["राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी"],
+["मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम"],
+["राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम"],
+["बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"],
+["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"],
+["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]];
+
+
+var comarray= ["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम","राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी","मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम","राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम","बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को","एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब","एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब","John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple","some students like to study in the night","at night some students like to study","John and Mary went to church","Mary and John went to church","John went to church after eating","after eating John went to church","John after eating went to church","did he go to market","he did go to market","the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics", "John goes to the library and studies","John studies and goes to the library", "John ate an apple so did she","she ate an apple so did John", "the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book", "I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her" ];
+
+//shuffling words of the sentence
+
+function select_language()
+{
+if (document.getElementById("language").value === "English")
+{
+  
+    
+                answers="";
+                document.getElementById("output").innerHTML = ""
+   
+                document.getElementById("demo2").innerHTML = "(select the buttons in proper order)"
+                document.getElementById("demo1").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words"
+            
+ r = Math.floor(Math.random()* array1.length); 
+    var jumbled = array1[r][0];
+    var j = shuffle(jumbled);
+    button_count=0;
+        word_count=0;
+    var button ="";
+    var l_button = "";
+    for(i=0;i<=j.length-1;i++){
+      val = j[i];
+      button = "  <button id='btn"+i+"' onclick='finals(this.id,this.value)' value='"+val+"'>"+val+"</button>  ";
+      l_button +=button;
+            button_count++
+           
     }
-    else if (selection == 'hindi' ){
-        text = "Form a sentence (Declarative or Interrogative or any other type) from the given words<br>(select the buttons in proper order)";
-        randomHindi();
-    }
-    else {
-        alert("Select language");
-    }
-    document.getElementById("message").innerHTML = text;
+    demo3.innerHTML = l_button.trim();
+    
+                
 }
-function randomEnglish() {
-    var sentance = sentanceEnglish[Math.floor(Math.random() * sentanceEnglish.length)];
-    var randomize = randomizeWords(sentance);
-    for (var i = 0; i < randomize.length; i++) {
-        document.getElementById("randomButtons").innerHTML += "<button class='sentanceButtons'>" + randomize[i] + "</button>";
+else if (document.getElementById("language").value === "Hindi")
+{
+                answers="";
+                //alert("hi");
+                document.getElementById("output").innerHTML = ""
+    
+                document.getElementById("demo2").innerHTML = "(select the buttons in proper order)"
+                document.getElementById("demo1").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words"
+               r = Math.floor(Math.random()* array2.length);
+    var jumbled = array2[r][0];
+    var j = shuffle(jumbled);
+     button_count=0;
+    word_count=0;
+    var button ="";
+    var l_button = "";
+    for(i=0;i<=j.length-1;i++){
+      val = j[i];
+      button= "  <button id='btn"+i+"'onclick='finals(this.id,this.value)' value='"+val+"'>"+val+"</button>  ";
+      l_button+=button;
+            button_count++;
     }
+    demo3.innerHTML = l_button.trim();
+  }
+
+else if (document.getElementById("language").value === "null")
+{
+                alert("Choose correct language")
+                document.getElementById("demo2").innerHTML = ""
+                document.getElementById("demo1").innerHTML = ""
+                document.getElementById("demo3").innerHTML = ""
+                document.getElementById("demo4").innerHTML = ""
+               
+            }
 }
-function randomHindi() {
-    var sentance = sentanceHindi[Math.floor(Math.random() * sentanceHindi.length)];
-    var randomize = randomizeWords(sentance);
-    for (var i = 0; i < randomize.length; i++) {
-        document.getElementById("randomButtons").innerHTML += "<button class='sentanceButtons'>" + randomize[i] + "</button>";
-    }
+var word_count;
+var button_count,r;
+var answers="";
+
+
+//hiding
+function hides(){
+  document.getElementById('demo10').innerHTML = "<center><button id='showansbtn' onclick='toggle()'>Get Correct Sentence</button></center>"
+  document.getElementById("output").innerHTML = "";
 }
-function randomizeWords(innerArray) {
-    var length = innerArray.length;
-    var temp, index;
-    while (length > 0) {
-        index = Math.floor(Math.random() * length);
-        length--;
-        temp = innerArray[length];
-        innerArray[length] = innerArray[index];
-        innerArray[index] = temp;
-    }
-    return innerArray;
+
+
+//togle
+function toggle(){
+    while(document.getElementById("output").innerHTML ==""){
+  
+    document.getElementById('demo10').innerHTML = "<center><button id='showansbtn' onclick='hides()'>hide correct answer</button></center>"
+  }
+}
+
+
+function shuffle(jumbled){
+  var jumble = jumbled.split(" ");
+  var i = jumble.length, temp, randomi;
+  while(0!==i){
+    randomi = Math.floor(Math.random()*i);
+    i -= 1;
+    temp = jumble[i];
+    jumble[i] = jumble[randomi];
+    jumble[randomi] = temp;
+  }
+  return jumble;
+}
+
+var finalsentence ="";
+function finals(id,value){
+  
+  document.getElementById("demo4").innerHTML = "Formed Sentence (after selecting words):";
+  finalsentence += value + " ";
+   document.getElementById("demo5").innerHTML = finalsentence;
+  document.getElementById(id).style.display = "none";
+  document.getElementById("demo6").innerHTML = "<center><button id='reform' onclick='resets()'>Re-form the sentence</button></center>"
+    word_count++;
+    
+}
+function resets(){
+  
+  document.getElementById("demo5").innerHTML = "";
+    
+    document.getElementById("demo6").innerHTML ="";
+  document.getElementById("demo4").innerHTML = "";
+    word_count=0;
 }
